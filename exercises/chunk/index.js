b9ca058a9be0d5feb60c6edd0146e9247b0ca6e4
thenatural86 <philip.smalls86@gmail.com>
2021-03-11 16:23:26 -0500

    notes

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,16 +9,24 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+  // create a new array for array chunks to go into
   let newArr = []
 
+  // iterate over array
   for (let element of array) {
+    // retrieve the last element in new Array and save to variable
     let last = newArr[newArr.length - 1]
+
+    // if last element doesn't exist or is its length is equal to size
     if (!last || last.length === size) {
+      // push element into new array
       newArr.push([element])
     } else {
+      // othewerwise add current element into last
       last.push(element)
     }
   }
+
   return newArr
 }
 
