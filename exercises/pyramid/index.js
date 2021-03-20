3cfac4fa3e7cb44dcfe08155bc6806a32fda3aa6
thenatural86 <philip.smalls86@gmail.com>
2021-03-20 10:38:18 -0400

    pyramid solve

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -15,10 +15,12 @@
 //       '#####'
 
 function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2)
+
   for (let row = 0; row < n; row++) {
     let level = ''
-    for (let col = 0; col < n + row - 1; col++) {
-      if (col <= row) {
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) {
         level += '#'
       } else {
         level += ' '
