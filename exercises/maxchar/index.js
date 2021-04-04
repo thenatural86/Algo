04653ec20d5bd171412be0cc54e93408b9488ad6
thenatural86 <philip.smalls86@gmail.com>
2021-04-04 12:01:26 -0400

    finished

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,7 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {}
+  let max = 0
+  let maxChar = 0
+
+  for (let char of str) {
+    charMap[char] = 1 || charMap[char]++
+  }
+
+  for (char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar
 //
