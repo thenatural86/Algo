3619f592553d0a3c4d9d1dc975c0eb0b591701ac
thenatural86 <philip.smalls86@gmail.com>
2021-04-05 18:04:13 -0400

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -23,34 +23,30 @@ function matrix(n) {
   }
 
   let counter = 1
-  let startCol = 0
-  let endCol = n - 1
   let startRow = 0
   let endRow = n - 1
+  let startCol = 0
+  let endCol = n - 1
 
   while (startCol <= endCol && startRow <= endRow) {
-    // Top row
     for (let i = startCol; i <= endCol; i++) {
       results[startRow][i] = counter
       counter++
     }
     startRow++
 
-    // Right column
     for (let i = startRow; i <= endRow; i++) {
       results[i][endCol] = counter
       counter++
     }
     endCol--
 
-    // Bottom row
     for (let i = endCol; i >= startCol; i--) {
       results[endRow][i] = counter
       counter++
     }
     endRow--
 
-    // Start column
     for (let i = endRow; i >= startRow; i--) {
       results[i][startCol] = counter
       counter++
@@ -61,3 +57,70 @@ function matrix(n) {
 }
 
 module.exports = matrix
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//  let results = []
+
+//  for (let i = 0; i < n; i++) {
+//    results.push([])
+//  }
+
+//  let counter = 1
+//  let startCol = 0
+//  let endCol = n - 1
+//  let startRow = 0
+//  let endRow = n - 1
+
+//  while (startCol <= endCol && startRow <= endRow) {
+//    // Top row
+//    for (let i = startCol; i <= endCol; i++) {
+//      results[startRow][i] = counter
+//      counter++
+//    }
+//    startRow++
+
+//    // Right column
+//    for (let i = startRow; i <= endRow; i++) {
+//      results[i][endCol] = counter
+//      counter++
+//    }
+//    endCol--
+
+//    // Bottom row
+//    for (let i = endCol; i >= startCol; i--) {
+//      results[endRow][i] = counter
+//      counter++
+//    }
+//    endRow--
+
+//    // Start column
+//    for (let i = endRow; i >= startRow; i--) {
+//      results[i][startCol] = counter
+//      counter++
+//    }
+//    startCol++
+//  }
+//  return results
