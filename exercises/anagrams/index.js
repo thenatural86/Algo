72d509b6c14c1bfe8ef16f3254c230a8a89eb1a6
thenatural86 <philip.smalls86@gmail.com>
2021-04-12 18:39:00 -0400

    algo update

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,30 +8,7 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  let map1 = helper(stringA.replace(/'[^w], g'/))
-  let map2 = helper(stringB.replace(/'[^w], g'/))
-
-  if (Object.keys(map1).length !== Object.keys(map2).length) {
-    return false
-  }
-
-  for (let char in map1) {
-    if (map1[char] !== map2[char]) {
-      return false
-    }
-  }
-  return true
-}
-
-const helper = (str) => {
-  let charMap = {}
-
-  for (let char of str) {
-    charMap[char] = 1 || charMap[char]++
-  }
-  return charMap
-}
+function anagrams(stringA, stringB) {}
 
 module.exports = anagrams
 
